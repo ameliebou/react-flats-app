fc872b7034265c10902649e3c148e11a9f721ca4
Amelie Boudier <amelie.boudier01@gmail.com>
2019-03-28 10:35:25 +0100

    created Map component with Google Map React

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,9 +7,9 @@ class App extends Component {
     super(props);
     this.state = {
       flats: [
-        { id: 1, name: "Charming flat close to the Spree", price: "99€", image: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fthespaces.com%2Fwp-content%2Fuploads%2F2017%2F01%2FBerlin-rental-Beautiful-sunny-loft-with-balconyUntitled-1.jpg&f=1" },
-        { id: 2, name: "Romantic apartment for a lover's weekend", price: "120€", image: "https://backwoodshousewife.com/wp-content/uploads/2018/08/Buy-Flat-Berlin.jpg" },
-        { id: 3, name: "Stylish loft", price: "150€", image: "https://pics.magicstay.com/photo-annonce-XJ/IO/M3/9A/bien_rentalsunited_110448_20181101001119_54.jpg" }
+        { id: 1, name: "Charming flat close to the Spree", price: "99€", image: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fthespaces.com%2Fwp-content%2Fuploads%2F2017%2F01%2FBerlin-rental-Beautiful-sunny-loft-with-balconyUntitled-1.jpg&f=1", lat: 52.51656, lng: 13.40519 },
+        { id: 2, name: "Romantic apartment for a lover's weekend", price: "120€", image: "https://backwoodshousewife.com/wp-content/uploads/2018/08/Buy-Flat-Berlin.jpg", lat: 52.49517, lng: 13.40126 },
+        { id: 3, name: "Stylish loft", price: "150€", image: "https://pics.magicstay.com/photo-annonce-XJ/IO/M3/9A/bien_rentalsunited_110448_20181101001119_54.jpg", lat: 52.52575, lng: 13.3306 }
       ]
     }
   }
@@ -21,7 +21,7 @@ class App extends Component {
           <FlatList flats={this.state.flats} />
         </div>
         <div className="right-scene">
-          <Map />
+          <Map flats={this.state.flats} />
         </div>
       </div>
     )
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
     return(
       <div className="map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          {this.props.flats.map (flat => {
+            <AnyReactComponent
+              lat={flat.lat}
+              lng={flat.lng}
+              text={flat.name}
+            />
+          })}
+        </GoogleMapReact>
       </div>
     )
   };
