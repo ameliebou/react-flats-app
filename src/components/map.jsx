d9a56b5816537c99c548b73fa869969c3e0fc352
Amelie Boudier <amelie.boudier01@gmail.com>
2019-04-02 17:25:41 +0200

    implemented map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import Marker from './marker.jsx'
 
 class Map extends Component {
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 52.5,
+      lng: 13.4
     },
-    zoom: 11
+    zoom: 12
   };
 
   render() {
     return(
       <div className="map">
         <GoogleMapReact
-          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          {this.props.flats.map (flat => {
-            <AnyReactComponent
-              lat={flat.lat}
-              lng={flat.lng}
-              text={flat.name}
-            />
-          })}
+          <Marker
+            lat={this.props.flat.lat}
+            lng={this.props.flat.lng}
+          />
         </GoogleMapReact>
       </div>
     )
